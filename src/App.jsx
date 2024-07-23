c1ffd6e7fbd8bf30278525bcbc9d3d34ead3fb65
Ravindra Kumar <ravindrakar9648@gmail.com>
2024-07-23 17:50:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Landing from './Components/Landing';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
